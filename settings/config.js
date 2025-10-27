8873ab7f75c9377b5038195bbea8b5650a08abae
Dravin <dravinnnnn@gmail.com>
2025-10-27 20:49:19 +0700

    Add files via upload

--- settings/config.js
+++ settings/config.js
@@ -1,1 +1,18 @@
+import fs from "fs";
+import chalk from "chalk";
 
+global.owner = ["6282199509537"];
+global.mode = false;
+
+global.namebotz = "Alifatah wabot !";
+global.pairing = "True"; // True atau false
+global.pairingcode = "DRAVINZZ" // 8 huruf
+global.packname = 'www.ziihost.store';
+global.nameown = "Fauzialifatah | Projects";
+global.author = 'https://www.github.com/Alifatahfauzi';
+global.footer = "𝗍𝖾𝗅𝖾𝗀𝗋𝖺𝗆: @FauziAlifatah";
+
+global.YouTube = "https://www.youtube.com/@Fauzialifatah";
+global.GitHub = "https://github.com/Alifatahfauzi";
+global.Telegram = "https://t.me/FauziAlifatah";
+global.ChannelWA = "https://whatsapp.com/channel/0029VawsCnQ9mrGkOuburC1z";
